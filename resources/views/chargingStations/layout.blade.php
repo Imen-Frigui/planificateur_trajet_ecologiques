0fac434c3bed76c759b5a1b7d4289cfe781e9ab7
Adoulii <omaradouli79@gmail.com>
2024-10-27 20:08:35 +0100

    ui improvements

---
+++ resources/views/chargingStations/layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="{{ route('weather.index') }}">All Weather Conditions</a></li>
+            <li><a href="{{ route('weather.create') }}">Create Weather Condition</a></li>
+        </ul>
+    </nav>
+    
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
